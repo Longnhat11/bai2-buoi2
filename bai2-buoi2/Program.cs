28923bcb7d2294967846af9abba569d09c07d978
Nhật Long <longnhat678@outlook.com>
2024-03-14 00:51:08 +0700

    la2

---
+++ bai2-buoi2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace bai2_buoi2
+{
+    internal class Program
+    {
+        static void Main(string[] args)           
+        {
+            Console.WriteLine("nhap vao mot chuoi: ");
+            string StringIn;
+            string StringOut=string.Empty;
+            StringIn= Console.ReadLine(); 
+            StringIn.ToList().ForEach(x=>StringOut=x+StringOut);  
+            Console.WriteLine(StringOut);
+            Console.ReadKey();
+        }
+    }
+}
